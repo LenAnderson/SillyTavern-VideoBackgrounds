aa33f9c9f430dfbe6bc5a16a4564e6143969a3d0
LenAnderson <Anderson.Len@outlook.com>
2023-12-16 22:11:16 +0000

    add fade transition

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
+import { delay } from "../../../utils.js";
+
 (function() {
-	'use strict';
-	const wait = async(millis)=>(new Promise(resolve=>setTimeout(resolve,millis)));
-	const debounce = (func, delay=100)=>{
-		let to;
-		return (...args) => {
-			if (to) clearTimeout(to);
-			to = setTimeout(()=>func.apply(this, args), delay);
-		};
-	}
+    'use strict';
+    const wait = async(millis)=>(new Promise(resolve=>setTimeout(resolve,millis)));
+    const debounce = (func, delay = 100)=>{
+        let to;
+        return (...args) => {
+            if (to) clearTimeout(to);
+            to = setTimeout(()=>func.apply(this, args), delay);
+        };
+    };
 
-	let videoEl;
+    let videoEl;
 
-	const replaceBg = async(muts=[])=>{
-		console.log('[STVBG]', 'replaceBg', muts);
-		await wait(1000);
-		const el = document.querySelector('#bg1');
-		const customEl = document.querySelector('#bg_custom');
-		const bg = window.getComputedStyle(el).background;
-		const bgUrl = bg.replace(/^.*?url\(['"]?([^'"\)]+)['"]\).*?$/, '$1');
-		const customBg = window.getComputedStyle(customEl).background
-		const customUrl = customBg.replace(/^.*?url\(['"]?([^'"\)]+)['"]\).*?$/, '$1');
-		let url;
-		if (customUrl && customUrl != customBg) {
-			url = customUrl;
-		} else if (bgUrl && bgUrl != bg) {
-			url = bgUrl;
-		}
-		console.log('[STVBG]', url);
-		if (url.match(/^.+\.[a-z0-z]+\.[a-z0-9]+$/i)) {
-			const vurl = url.replace(/^(.+\.[a-z0-z]+)\.[a-z0-9]+$/i, '$1');
-			console.log('[STVBG]', vurl);
-			const resp = await fetch(url, {
-				method: 'HEAD',
-			});
-			console.log('[STVBG]', resp);
-			if (resp.ok) {
-				if (!videoEl) {
-					const v = document.createElement('video'); {
-						v.loop = true;
-						v.autoplay = true;
-						v.muted = true;
-						v.style.position = 'absolute';
-						v.style.height = '100%';
-						v.style.width = '100%';
-						v.style.objectFit = 'cover';
-					}
-					videoEl = v;
-				}
-				videoEl.src = vurl;
-				(customEl ?? el).append(videoEl);
-				while (true) {
-					try {
-						await videoEl.play();
-						break;
-					} catch(ex) {
-						await wait(100);
-					}
-				}
+    const replaceBg = async(muts = [])=>{
+        console.log('[STVBG]', 'replaceBg', muts);
+        await wait(1000);
+        const el = document.querySelector('#bg1');
+        const customEl = document.querySelector('#bg_custom');
+        const bg = window.getComputedStyle(el).background;
+        const bgUrl = bg.replace(/^.*?url\(['"]?([^'"\)]+)['"]\).*?$/, '$1');
+        const customBg = window.getComputedStyle(customEl).background;
+        const customUrl = customBg.replace(/^.*?url\(['"]?([^'"\)]+)['"]\).*?$/, '$1');
+        let url;
+        if (customUrl && customUrl != customBg) {
+            url = customUrl;
+        } else if (bgUrl && bgUrl != bg) {
+            url = bgUrl;
+        }
+        console.log('[STVBG]', url);
+        if (url.match(/^.+\.[a-z0-z]+\.[a-z0-9]+$/i)) {
+            const vurl = url.replace(/^(.+\.[a-z0-z]+)\.[a-z0-9]+$/i, '$1');
+            console.log('[STVBG]', vurl);
+            const resp = await fetch(url, {
+                method: 'HEAD',
+            });
+            console.log('[STVBG]', resp);
+            if (resp.ok) {
+                const v = document.createElement('video'); {
+                    v.loop = true;
+                    v.autoplay = true;
+                    v.muted = true;
+                    v.style.position = 'absolute';
+                    v.style.height = '100%';
+                    v.style.width = '100%';
+                    v.style.objectFit = 'cover';
+                    v.style.opacity = '0';
+                    v.style.transition = '400ms linear';
+                    v.src = vurl;
+                }
+                (customEl ?? el).append(v);
+                await delay(20);
+                v.style.opacity = '1';
+                while (true) {
+                    try {
+                        await videoEl.play();
+                        break;
+                    } catch(ex) {
+                        await wait(100);
+                    }
+                }
+                await delay(450);
+                videoEl?.remove();
+                videoEl = v;
 
-			}
-		} else if (videoEl) {
-			videoEl.remove();
-		}
-	};
-	replaceBg();
+            }
+        } else if (videoEl) {
+            videoEl.remove();
+        }
+    };
+    replaceBg();
 
-	const mo = new MutationObserver(debounce(replaceBg));
-	mo.observe(document.querySelector('#bg1'), {attributes:true});
-	mo.observe(document.querySelector('#bg_custom'), {attributes:true});
+    const mo = new MutationObserver(debounce(replaceBg));
+    mo.observe(document.querySelector('#bg1'), { attributes:true });
+    mo.observe(document.querySelector('#bg_custom'), { attributes:true });
 })();
 
 $(document).ready(function () {
-	const addSettings = () => {
-		const html = `
+    const addSettings = () => {
+        const html = `
 		<div class="vbg--settings">
 			<div class="inline-drawer">
 				<div class="inline-drawer-toggle inline-drawer-header">
@@ -100,7 +106,7 @@ $(document).ready(function () {
 			</div>
 		</div>
 		`;
-		$('#extensions_settings').append(html);
-	};
-	addSettings();
+        $('#extensions_settings').append(html);
+    };
+    addSettings();
 });
